ad1854379e481f050af1dd65ec95855bda73b0af
15828449889@qq.com <1528449889@qq.com>
2023-12-10 20:06:19 +0800

    tttttttest

--- utils/core_utils.py
+++ utils/core_utils.py
@@ -264,7 +264,7 @@ def train_loop_clam(epoch, model, loader, optimizer, n_classes, bag_weight, writ
     print('\n')
     for batch_idx, (data, label) in enumerate(loader):
         data, label = data.to(device), label.to(device)
-        logits, Y_prob, Y_hat, _, instance_dict = model(data, label=label, instance_eval=True, use_cluster=True)
+        logits, Y_prob, Y_hat, _, instance_dict = model(data, label=label, instance_eval=True)
 
         acc_logger.log(Y_hat, label)
         loss = loss_fn(logits, label)
